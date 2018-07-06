cb7438099fa88664a62ddd5e530a078a4aed895f
BAAAKA <roy.aregger@protonmail.ch>
2018-07-06 10:42:56 +0200

    Not working yet

--- src/Vwin/Vwin_GUI.java
+++ src/Vwin/Vwin_GUI.java
@@ -1,5 +1,10 @@
 package Vwin;
 
 public class Vwin_GUI {
-
+	Vwin_Logic l=new Vwin_Logic();
+	
+	public Vwin_GUI(){
+		l.whereCircle(0);
+		l.whereCircle(0);
+	}
 }
--- src/Vwin/Vwin_Logic.java
+++ src/Vwin/Vwin_Logic.java
@@ -1,5 +1,54 @@
 package Vwin;
 
-public class Vwin_Logic {
+import java.util.Arrays;
 
+public class Vwin_Logic {
+	/*
+	 * 0 : Empty
+	 * 1 : Red
+	 * 2 : Yellow
+	 */
+	int[] place=new int[42];
+	int color=0;
+	
+	public Vwin_Logic(){
+		Arrays.fill(place, 0);
+		this.color=1;
+	}
+	public void whereCircle(int row){
+		if(place[row]!=0){
+			System.out.println("Error, row is full");
+		}
+		int position=place[row];
+		while(true){
+			if(position<=34){
+				if(place[position+7]==0){
+					position+=7;
+				}
+			}
+			else{
+				setCircle(position, color);
+				changeColor(color);
+				break;
+			}
+		}
+		
+	}
+	private void setCircle(int position, int color){
+		place[position]=color;
+		System.out.println("Setting Circle on: " + position +" color: "+ color);		
+	}
+	private void changeColor(int color){
+		if(color==1){
+			this.color=2;
+			System.out.println("Changing Color to Yellow");
+		}else if(color==2){
+			this.color=1;
+			System.out.println("Changing Color to Red");
+		}
+	}
+	private void firstMove(){
+		this.color=1;
+	}
+	
 }
